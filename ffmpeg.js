d321fdb74d1320b780e35a2255f79d8d7a997684
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-01-21 18:37:20 -0800

    REFACTORED

---
+++ ffmpeg.js
@@ -0,0 +1,28 @@
+var ffmpeg = require('ffmpeg');
+
+module.exports = {
+  extraImagesFromVideo: function(videoURL, callback) {
+    try {
+      var process = new ffmpeg(videoURL);
+      process.then( function (video) {
+        video.fnExtractFrameToJPG('./videos', {
+          frame_rate : 1,
+          file_name : 'my_frame_%t_%s',
+          every_n_seconds: 1,
+        }, 
+        function (error, files) {
+          if (!error)
+            callback(files);
+        });
+      }, 
+      function (err) {
+        console.log('Error: ' + err);
+      });
+    } 
+    catch (e) {
+      console.log(e.code);
+      console.log(e.msg);
+    }
+  }
+
+};
--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-/*
-var tesseract = require('node-tesseract');
- 
-// Recognize text of any language in any format
-tesseract.process(__dirname + '/screenshot.png', function(err, text) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(text);
-    }
+var ffmpegLogic = require("./ffmpeg.js");
+var tesseractLogic = require("./tesseract.js");
+
+ffmpegLogic.extraImagesFromVideo('cse100.mp4', function (fileNames) {
+  extractWithTesseract(fileNames[0], function (text) {
+    console.log(text);
+  });
 });
+/*
+
  
  */
-var ffmpeg = require('ffmpeg');
 
 
-try {
-  var process = new ffmpeg('cse100.mp4');
-  process.then(
-    function (video) {
-      video.fnExtractFrameToJPG('./videos', {
-        frame_rate : 1,
-        file_name : 'my_frame_%t_%s'
-      }, 
-      function (error, files) {
-        if (!error)
-          console.log('Frames: ' + files);
-      }
-    );
-  }, 
-  function (err) {
-    console.log('Error: ' + err);
-  });
-} 
-catch (e) {
-  console.log(e.code);
-  console.log(e.msg);
-}
+
