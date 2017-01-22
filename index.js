af87175214a25781c06f35f01f75c333ceb3fca4
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-01-21 18:26:06 -0800

    Did ffmpeg stuff

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/*
 var tesseract = require('node-tesseract');
  
 // Recognize text of any language in any format
-tesseract.process(__dirname + '/screenshot.png',function(err, text) {
+tesseract.process(__dirname + '/screenshot.png', function(err, text) {
     if(err) {
         console.error(err);
     } else {
@@ -9,25 +10,29 @@ tesseract.process(__dirname + '/screenshot.png',function(err, text) {
     }
 });
  
+ */
 var ffmpeg = require('ffmpeg');
 
 
 try {
   var process = new ffmpeg('cse100.mp4');
-  process.then(function (video) {
-    // Callback mode
-    video.fnExtractFrameToJPG('/path/to/save_your_frames', {
-      frame_rate : 1,
-      number : 5,
-      file_name : 'my_frame_%t_%s'
-    }, function (error, files) {
-      if (!error)
-        console.log('Frames: ' + files);
-    });
-  }, function (err) {
+  process.then(
+    function (video) {
+      video.fnExtractFrameToJPG('./videos', {
+        frame_rate : 1,
+        file_name : 'my_frame_%t_%s'
+      }, 
+      function (error, files) {
+        if (!error)
+          console.log('Frames: ' + files);
+      }
+    );
+  }, 
+  function (err) {
     console.log('Error: ' + err);
   });
-} catch (e) {
+} 
+catch (e) {
   console.log(e.code);
   console.log(e.msg);
 }
