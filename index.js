6ff6d970b1bda586b9be7fe71256da3ea0f925f7
anupkrish24 <anupkrish24@gmail.com>
2017-01-21 20:23:08 -0800

    combined

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var ffmpegLogic = require("./ffmpeg.js");
 var tesseractLogic = require("./tesseract.js");
+var im = require("imagemagick");
+var http = require("http");
 var parseText = require("./parseText.js");
 
 ffmpegLogic.extraImagesFromVideo('cse100.mp4', function (fileNames) {
@@ -9,8 +11,24 @@ ffmpegLogic.extraImagesFromVideo('cse100.mp4', function (fileNames) {
 
 function recursivelyExtractWithTesseract (index, fileNameArrays) {
   console.log(fileNameArrays[index]);
-  tesseractLogic.extractWithTesseract(fileNameArrays[index], function (text) {
+  var newStr = "output" + index + ".png";
+  var args = [
+  	fileNameArrays[index],
+  	"-crop",
+  	"2300x1350+120+150",
+  	newStr,
+  ]
+  im.convert(args, function(err) {
+  	if (err) { console.log(err); }
+  	tesseractLogic.extractWithTesseract("./" + newStr, function (text) {
+    console.log(text);
     parseText.parseText(text);
     // recursivelyExtractWithTesseract(index + 1, fileNameArrays);
+  	});
   });
 }
+
+// var exec =
+// tesseractLogic.extractWithTesseract('./output0.png', function(text) {
+// 	console.log(text);
+// });
