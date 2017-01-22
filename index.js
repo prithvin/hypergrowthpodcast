be464214223139f6f630717b12afe3616f88479f
anupkrish24 <anupkrish24@gmail.com>
2017-01-21 21:05:59 -0800

    Refactored + working

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-
-var ffmpegLogic = require("./ffmpeg.js");
-var tesseractLogic = require("./tesseract.js");
-var im = require("imagemagick");
-var http = require("http");
-var parseText = require("./parseText.js");
 var exec = require("child_process").exec;
-var normcmd = './textcleaner';
+var ffmpegLogic = require("./ffmpeg.js");
 var deletecmd = 'rm -rf ./videos';
+var normalize = require("./normalizeImage.js");
 
-// ffmpegLogic.extraImagesFromVideo('cse100.mp4', function (fileNames) {
-//   recursivelyExtractWithTesseract(0, fileNames);
-// });
+ffmpegLogic.extraImagesFromVideo('cse100.mp4', function (fileNames) {
+  recursivelyExtractWithTesseract(0, fileNames);
+  exec(deletecmd, function(error, stdout, stderr) {
+    console.log(stdout);
+    console.log(error);
+    console.log(stderr);
+  });
+});
 
-// function recursivelyExtractWithTesseract (index, fileNameArrays) {
-//   console.log(fileNameArrays[index]);
-//   var newStr = "output" + index + ".png";
-//   var args = [
-//   	fileNameArrays[index],
-//   	"-crop",
-//   	"2300x1350+120+150",
-//   	newStr,
-//   ]
-//   im.convert(args, function(err) {
-//   	if (err) { console.log(err); }
-//   	tesseractLogic.extractWithTesseract("./" + newStr, function (text) {
-//     console.log(text);
-//     parseText.parseText(text);
-//     // recursivelyExtractWithTesseract(index + 1, fileNameArrays);
-//   	});
-//   });
-// }
+function recursivelyExtractWithTesseract (index, fileNameArrays) {
+  normalize.normalizeImage(fileNameArrays[index], index, function(parsedString, text) {
+    console.log(parsedString);
+    console.log(text);
+    recursivelyExtractWithTesseract(index + 1, fileNameArrays);
+  })
+}
 
 // var exec =
-tesseractLogic.extractWithTesseract('./o1.png', function(text) {
-  console.log('1');
-	console.log(text);
-});
+// tesseractLogic.extractWithTesseract('./o1.png', function(text) {
+//   console.log('1');
+// 	console.log(text);
+// });
 
 // tesseractLogic.extractWithTesseract('./o5.png', function(text) {
 //     console.log('5');
