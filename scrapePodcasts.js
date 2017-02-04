d9421b3e22dec3af70dc9b42f7a557267678b64a
Kevin Li <likevin2010@gmail.com>
2017-02-03 21:37:29 -0800

    scraping to find new podcasts that have appeared

--- scrapePodcasts.js
+++ scrapePodcasts.js
@@ -5,9 +5,23 @@ var fs = require('fs');
 module.exports = {
     scrapePodcasts: function(callback) {
         exec('./scrape_podcasts.py', function(error, stdout, stderr) {
-            var urls = stdout.split("\n");
-            fs.writeFile("scraped",
-            JSON.stringify(urls), "utf8", callback);
+            var arr = stdout.split("\n").filter(Boolean);
+            var urls = new Set(arr);
+
+            fs.readFile("scraped", function read(err, data) {
+                if (err) {
+                    throw err;
+                }
+
+                var old = new Set(JSON.parse(data));
+                var diff = arr.filter(x => !old.has(x));
+                console.log("new podcast list");
+                console.log(diff);
+
+                fs.writeFile("diff", JSON.stringify(diff), "utf8", callback);
+                fs.writeFile("scraped", JSON.stringify(arr), "utf8");
+            });
+
         });
     }
 }
