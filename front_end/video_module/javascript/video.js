153e5f8520f31927d81f65ea41b5509bbbe4e442
Allen Wan <allenwan96@gmail.com>
2017-02-10 15:12:45 -0800

    source input is now list of video series and index of current video

--- front_end/video_module/javascript/video.js
+++ front_end/video_module/javascript/video.js
@@ -1,9 +1,13 @@
 
 class videoClass {
-
+    
+    
+    
     constructor (urlParams) {
         this.urlParams = urlParams;
-        this.setSource(this.getDataOrDefault("source"));
+        this.initSourceArray(this.getDataOrDefault("source"));
+
+        this.setSource(this.getDataOrDefault("index"));
         this.setTime(this.getDataOrDefault("time"));
 
         this.initHotKeys();
@@ -16,11 +20,28 @@ class videoClass {
             return "N/A";
         return decodeURIComponent(this.urlParams[key]);
     }
-
-    setSource(source){
-        $("#videosrc").attr("src", source);
-    }
     
+    initSourceArray(source){
+        this.sourceArray = JSON.parse(source);
+        console.log("loaded video series of length " + this.sourceArray.length);
+    }
+
+    setSource(index){
+        if(index < 0){
+            this.index = 0;
+            console.log("VIDEO SOURCE INDEX OUT OF BOUNDS");
+        }
+        else if(index >= this.sourceArray.length){
+            this.index = this.sourceArray.length - 1;
+            console.log("VIDEO SOURCE INDEX OUT OF BOUNDS");
+        }
+        else{
+            this.index = index;
+        }
+        console.log("loading: " + this.sourceArray[this.index]);
+        $("#videosrc").attr("src", this.sourceArray[this.index]);
+
+    }
     initHotKeys(){
         document.addEventListener('videojsLoaded', function(e){
             videojs('my-video').ready(function() {
