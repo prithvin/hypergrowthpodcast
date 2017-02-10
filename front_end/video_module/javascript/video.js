5a4957fa3394d57437f2cc71615a0d8495173225
codyndoan <c8doan@ucsd.edu>
2017-02-10 13:52:44 -0800

    prev and next buttons

--- front_end/video_module/javascript/video.js
+++ front_end/video_module/javascript/video.js
@@ -1,8 +1,10 @@
 class videoClass {
     constructor (urlParams) {
         this.urlParams = urlParams;
-        this.setSource(this.getDataOrDefault("source"));
+        this.setSource(this.getDataOrDefault("source"),this.getDataOrDefault("previous"),this.getDataOrDefault("next"));
         this.setTime(this.getDataOrDefault("time"));
+        this.nextVid(this.getDataOrDefault("next"));
+        this.prevVid(this.getDataOrDefault("previous"));
     }
     
     getDataOrDefault (key) {
@@ -11,8 +13,13 @@ class videoClass {
         return decodeURIComponent(this.urlParams[key]);
     }
 
-    setSource(source){
+    setSource(source, previous, next){
         $("#videosrc").attr("src", source);
+        /*$("#videosrc").src([
+            { type: "video/mp4", src: "source" },
+            { type: "video/mp4", src: "previous" },
+            { type: "video/mp4", src: "next" }
+        ]);*/
     }
     
     setTime(time){
@@ -28,6 +35,26 @@ class videoClass {
             console.log(currentTime);
         });
     }
+    
+    nextVid(next) {
+        /*document.getElementById("next").addEventListener("click", function() {
+            var video = document.getElementById('my-video');
+            $("#videosrc").attr("src", next);
+            video.load();
+            console.log(document.getElementById("videosrc").src);
+        });*/
+    }
+    
+    prevVid(previous) {
+        /*document.getElementById("prev").addEventListener("click", function() {
+            var video = document.getElementById('my-video');
+            $("#videosrc").attr("src", previous);
+            video.load();
+            console.log(document.getElementById("videosrc").src);
+        });*/
+    }
+    
+    
                             
                                                         
     
