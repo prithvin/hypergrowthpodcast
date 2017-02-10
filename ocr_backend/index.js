9b39db14a92ded7684a440bbc2c79894bdd6ce86
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-02-09 21:28:15 -0800

    Db uploaded crap

--- ocr_backend/index.js
+++ ocr_backend/index.js
@@ -3,7 +3,7 @@ var scraper = require("./scrapePodcasts.js");
 var videoParsing = require("./videoParsing.js");
 var fs = require('fs');
 
-
+var uploader = require('./dbuploader.js');
 
 //console.log(levenshtein.levenshteinDistance('designing justice does law alone create justice war of all against all in urban colombia construction ofjustice in bogota and medellin remaking the culture remaking the built environment evidence broken window theory of norm compliance medellin data'
 //  , 'designing justice does law alone create justice war of all against all in urban colombia construction ofjustice in bogota and medellin remaking the culture remaking the built environment evidence broken window theory of norm compliance medellin data'));
@@ -22,6 +22,15 @@ fs.stat("scraped", function(err, stats) {
 });
 */
 // In production, we would read the array from JSON.parse(diff file)
-videoParsing.parseVideo(['http://podcast.ucsd.edu/Podcasts//fa16/poli27fa16/poli27fa16-11022016-1200.mp4',
+/*videoParsing.parseVideo(['http://podcast.ucsd.edu/Podcasts//fa16/poli27fa16/poli27fa16-11022016-1200.mp4',
 'http://podcast.ucsd.edu/Podcasts//cse101_1_wi17/cse101_1_wi17-02032017-0900.mp4'],
-0);
+0);*/
+
+
+uploader.addPodcast({}, function (data) {
+    console.log(data);
+})
+
+uploader.addSlide({}, function (data) {
+    console.log(data);
+})
