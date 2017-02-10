c7ff1f6b316f094ae36a42f6f650a08800710e6f
Allen Wan <allenwan96@gmail.com>
2017-02-10 13:47:31 -0800

    fixed async with videojs

--- front_end/video_module/javascript/video.js
+++ front_end/video_module/javascript/video.js
@@ -1,11 +1,16 @@
+
 class videoClass {
+
     constructor (urlParams) {
         this.urlParams = urlParams;
         this.setSource(this.getDataOrDefault("source"));
-        
         this.setTime(this.getDataOrDefault("time"));
+
+        this.initHotKeys();
     }
     
+                     
+        
     getDataOrDefault (key) {
         if (this.urlParams[key] == null)
             return "N/A";
@@ -15,10 +20,25 @@ class videoClass {
     setSource(source){
         $("#videosrc").attr("src", source);
     }
+    
+    initHotKeys(){
+        document.addEventListener('videojsLoaded', function(e){
+            videojs('my-video').ready(function() {
+                console.log("loading hotkeys");
+                this.hotkeys({
+                    volumeStep: 0.1,
+                    seekStep: 5,
+                    enableModifiersForNumbers: false
+                });
+            });
+        });
+        
+    }
+    
     setTime(time){
-        document.getElementById("my-video").addEventListener("loadedmetadata", function() {
-            this.currentTime = time;
-        }, false);
+            document.getElementById("my-video").currentTime = time;
+       
+        
     }
 
 }
