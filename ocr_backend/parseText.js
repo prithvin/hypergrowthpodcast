5546dc2bad838b77fb548bb334a5afda6f4593c1
Brian Ng <brian.ng8828@gmail.com>
2017-02-10 19:27:54 -0800

    keyword working

--- ocr_backend/parseText.js
+++ ocr_backend/parseText.js
@@ -1,31 +1,46 @@
 var retext = require('retext');
 var nlcstToString = require('nlcst-to-string');
 var keywords = require('retext-keywords');
-var worder = require("worder");
+var worder = require('worder');
 var checkWord = require('check-word');
 var checker = checkWord('en');
 var countWords = require("count-words");
 var read = require('file-reader');
 var fs = require('fs-extra');
 
+const SLIDE_TITLE_KEYWORD_WEIGHT = 25;
+Object.prototype.getName = function() { 
+   var funcNameRegex = /function (.{1,})\(/;
+   var results = (funcNameRegex).exec((this).constructor.toString());
+   return (results && results.length > 1) ? results[1] : "";
+};
+
 module.exports = {
     parseText: function(dir) {
+
         var files = read(dir + '/*.txt');
         for (var file in files) {
-            var words = countWords(files[file]);
+            var fileBuffer = files[file].contents;
+            if (fileBuffer != undefined) {
+                // console.log(file);
+                // console.log(fileBuffer.toString());
+                var text = fileBuffer.toString();
+                var words = countWords(text);
 
-            // Put extra weight on title keywords
-            var splitted = files[file].split("\n");
-            var firstLine = worder(splitted[0]);
+                // Put extra weight on title keywords
+                var splitted = text.split("\n");
+                var firstLine = worder(splitted[0] + '');
 
-            for (var i = 0; i < firstLine.length; i++) {
-                var word = firstLine[i];
-                if (words[word] != null) words[word] += 25;
+                for (var i = 0; i < firstLine.length; i++) {
+                    var word = firstLine[i];
+                    if (words[word] != null) words[word] += SLIDE_TITLE_KEYWORD_WEIGHT;
+                }
+
+                fs.writeJson('./contents/' + file + '_keywords.txt', words, function(err) {
+                    if (err != null) console.log(err);
+                    // else console.log("...Parsed keywords for " + file);
+                });
             }
-
-            fs.writeJson('./' + file + '_Keywords', words, function(err) {
-                console.log("...Parsed keywords for " + file);
-            });
         }
         
        //var counts = countWords(text);
