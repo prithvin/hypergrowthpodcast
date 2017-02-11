41230e887a6860bce5996e37773e0ea55a6d24b8
Brian Ng <brian.ng8828@gmail.com>
2017-02-10 19:32:41 -0800

    cleanup

--- ocr_backend/parseText.js
+++ ocr_backend/parseText.js
@@ -9,11 +9,6 @@ var read = require('file-reader');
 var fs = require('fs-extra');
 
 const SLIDE_TITLE_KEYWORD_WEIGHT = 25;
-Object.prototype.getName = function() { 
-   var funcNameRegex = /function (.{1,})\(/;
-   var results = (funcNameRegex).exec((this).constructor.toString());
-   return (results && results.length > 1) ? results[1] : "";
-};
 
 module.exports = {
     parseText: function(dir) {
@@ -22,8 +17,6 @@ module.exports = {
         for (var file in files) {
             var fileBuffer = files[file].contents;
             if (fileBuffer != undefined) {
-                // console.log(file);
-                // console.log(fileBuffer.toString());
                 var text = fileBuffer.toString();
                 var words = countWords(text);
 
@@ -42,39 +35,5 @@ module.exports = {
                 });
             }
         }
-        
-       //var counts = countWords(text);
-        //console.log(counts);
-        // var words = worder(text);
-        // var validWords = new Array();
-
-        // for (var i = 0; i < words.length; i++) {
-        //     var word = words[i];
-        //     if (checker.check(word)) {
-        //         validWords.push(word);
-        //     }
-        // }
-        // console.log(validWords);
-
-        // console.log();
-        // console.log("---------------------Retext-Keywords--------------------------")
-        // console.log();
-        // retext().use(keywords).process(
-        //   text,
-        //   function (err, file) {
-        //     console.log('Keywords:');
-         
-        //     file.data.keywords.forEach(function (keyword) {
-        //       console.log(nlcstToString(keyword.matches[0].node));
-        //     });
-         
-        //     console.log();
-        //     console.log('Key-phrases:');
-         
-        //     file.data.keyphrases.forEach(function (phrase) {
-        //       console.log(phrase.matches[0].nodes.map(nlcstToString).join(''));
-        //     });
-        //   }
-        // );
     } 
 }
