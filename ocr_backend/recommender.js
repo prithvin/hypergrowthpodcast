06b70cd8a42857a0cf48cdd5ffc84e196cdf4d8d
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-02-13 15:26:31 -0800

    Recommender changes commited again

--- ocr_backend/recommender.js
+++ ocr_backend/recommender.js
@@ -6,12 +6,23 @@ module.exports = {
 		var tmp = podcastList.slice(podcastList);
 		var ret = [];
 
+    var indexOfInputPodcast = 0;
     for(var i = 0; i < tmp.length; i++) {
       if(String(podcastList[i]['_id']) === String(input_podcast['_id'])) {
         tmp.splice(i, 1);
+        indexOfInputPodcast = i;
         break;
       }
     }
+
+    var indexOfNextPodcast = indexOfInputPodcast + 1;
+    var indexOfPrevPodcast = indexOfInputPodcast - 1;
+    
+    if (indexOfPrevPodcast < 0)
+      indexOfPrevPodcast = podcastList.length - 1;
+
+    if (indexOfNextPodcast >= podcastList.length)
+      indexOfNextPodcast = 0;
 
 		while(tmp.length > 0 && ret.length < 5) {
 			var index = Math.floor(Math.random() * tmp.length);
@@ -21,6 +32,6 @@ module.exports = {
       ret.push(removed[0]);
     }
 
-    callback(ret);
+    callback(ret, indexOfPrevPodcast, indexOfNextPodcast);
 	}
 }
