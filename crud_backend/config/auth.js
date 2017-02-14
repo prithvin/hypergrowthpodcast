1c3b3b219dc743aae919f3cf91ac98e2d5b81801
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-02-13 17:45:51 -0800

    Fixed alan's package json

--- crud_backend/config/auth.js
+++ crud_backend/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   facebookAuth : {
-    clientID: 1676187752676007,
-    clientSecret: '54c5493014bcd38158ee6bf68c1f849f',
+    clientID: 1614405215488970,
+    clientSecret: '1a87f86706e98a18a82708c11ab6b4fc',
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
   }
 }
