564b2ff7015b18a0c8b08805cc802c246944bb1a
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-02-13 23:07:54 -0800

    Facebook auth token fixed

--- crud_backend/config/auth.js
+++ crud_backend/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   facebookAuth : {
-    clientID: 1614405215488970,
-    clientSecret: '1a87f86706e98a18a82708c11ab6b4fc',
+    clientID: 1606385449650440,
+    clientSecret: 'ee8e4d15e9a735deb9257d22372e89de',
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
   }
 }
