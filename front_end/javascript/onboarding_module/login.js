6d4822371ec27e911dbd90deeddf9c854ab2ae0b
Matthew Ho <itsmatho@gmail.com>
2017-02-17 19:52:07 -0800

    login

--- front_end/javascript/onboarding_module/login.js
+++
@@ -1,3 +0,0 @@
-$('.fb-login-button')[0].onclick = () => {
-  window.location.href = login_origins.backend + '/auth/facebook?callbackURL=' + login_origins.callback + '/onboarding_courses_module.html&errorCallbackURL=http://yahoo.com';
-}
