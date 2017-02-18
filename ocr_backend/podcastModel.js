6343b0db96cd2152d8291e22075e22a549b6a200
Kevin Li <likevin2010@gmail.com>
2017-02-17 22:18:58 -0800

    minor stuff dealing with models

--- ocr_backend/podcastModel.js
+++ ocr_backend/podcastModel.js
@@ -1,32 +1,30 @@
 // Create a schema
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var PodcastModel = new mongoose.Schema({
-  ClassName: String,
-  QuarterOfCourse: String,
-  ClassNameCourseKey: String,
-  VideoDate: String,
-  NextVideo: String,
-  PrevVideo: String,
-  PodcastName: String,
-  PodcastUrl: String,
-  PodcastImage: String,
-  OCRTranscription: String,
-  OCRTranscriptionFreq: [{Word: String, Frequency: Number}],
-  AudioTranscription: String,
-  AudioTranscriptionFreq: [{Word: String, Frequency: Number}],
-  Slides:[{SlideID:String, TimeStart:String,
-            OCRForSlide:String, AudioTranscription:String}],
-  LecturePost:[String],
-  RecommendedVideos: [
-		{
-			PodcastId: Number,
-			PodcastImage: String,
-			PodcastName: String
-		}
-	]
+	CourseId: Schema.Types.ObjectId, // id for course it belongs to
+	Time: Number,
+	PodcastUrl: String,
+	Slides: [{
+		SlideNum: Number,
+		OCRTranscription: String,
+		StartTime: Number,
+	}],
+	AudioTranscript: [{
+		StartTime: Number,
+		Content: String
+	}],
+	SRTBlob: String,
+	PrevVideo: Schema.Types.ObjectId,
+	NextVideo: Schema.Types.ObjectId,
+	Recommendations: [{
+		PodcastId: Schema.Types.ObjectId,
+		PodcastImage: String,
+		Time: Number
+	}]
 });
 
 // Create a model based on the schema
 var PodcastModel = mongoose.model('PodcastModel', PodcastModel);
 
-module.exports = {PodcastModel: PodcastModel}
+module.exports = {PodcastModel: PodcastModel};
