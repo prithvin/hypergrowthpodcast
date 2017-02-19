c69921e1655e1550c43d1b7d6ba2a42c75b14229
Kevin Li <likevin2010@gmail.com>
2017-02-18 21:31:14 -0800

    fix regex for course name in ocr_backend

--- ocr_backend/videoParsing.js
+++ ocr_backend/videoParsing.js
@@ -71,7 +71,7 @@ function parseFileNameForCourseData (fileName) {
 
   var formattedCourse = course.split('_')[0].toUpperCase();
   var pre = formattedCourse.split(/[0-9]/)[0];
-  formattedCourse = pre + ' ' + formattedCourse.slice(pre.length);
+  formattedCourse = pre + ' ' + formattedCourse.match(/\d+/)[0];
 
   return {
     "Date": dateinMillis,
