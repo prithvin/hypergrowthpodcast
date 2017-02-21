bc3d570d00523014930d889039ed04aff91f4d54
Matthew Ho <itsmatho@gmail.com>
2017-02-21 08:51:03 -0800

    onboarding error callback url

--- front_end/javascript/onboarding_module/onboarding.js
+++ front_end/javascript/onboarding_module/onboarding.js
@@ -1,7 +1,7 @@
 var Onboarding = class Onboarding {
   constructor(mainDiv) {
     $(mainDiv).find('.fb-login-button')[0].onclick = () => {
-      window.location.href = login_origins.backend + '/auth/facebook?callbackURL=' + login_origins.callback + '/%23/courses&errorCallbackURL=' + login_origins.callback;
+      window.location.href = login_origins.backend + '/auth/facebook?callbackURL=' + login_origins.callback + '/%23/courses&errorCallbackURL=' + login_origins.callback + '/%23/';
     }
   }
 }
