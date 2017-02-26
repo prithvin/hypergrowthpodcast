6b246ce512da017561367722c2e3b7aaed6fb8fa
Matthew Ho <itsmatho@gmail.com>
2017-02-25 17:20:55 -0800

    redirect query strings on onboarding

--- front_end/javascript/onboarding_module/onboarding.js
+++ front_end/javascript/onboarding_module/onboarding.js
@@ -7,18 +7,52 @@ var Onboarding = class Onboarding {
       window.location.href = login_origins.backend + '/auth/facebook?callbackURL=' + targetCallbackURL + '&errorCallbackURL=' + errorCallbackURL;
     }
 
-    callAPI(login_origins.backend + '/isUserLoggedIn', 'GET', {}, (data) => {
-      if(data.result === true) {
-        window.location.hash =  '/courses';
-      } else {
-        callAPI(login_origins.backend + '/getUserSession', 'GET', {}, (data) => {
-          if(data['user']) {
-            callAPI(login_origins.backend + '/setUserFromSession', 'GET', {}, (data) => {
-              window.location.hash =  '/courses';
-            });
-          }
-        });
+    var queries;
+		var query_str = window.location.search.substring(1);
+    if(!query_str) {
+      var query_start = window.location.href.indexOf('?');
+      if(query_start > 0) {
+        query_str = window.location.href.substring(query_start + 1);
+        queries = this.getQueries(query_str);
       }
-    });
+    } else {
+      queries = this.getQueries(query_str);
+    }
+
+    if(queries && queries.hasOwnProperty('redirectURL')) {
+      window.location.href = encodeURI(window.location.origin + '/#/' +  queries['redirectURL']);
+    } else {
+      callAPI(login_origins.backend + '/isUserLoggedIn', 'GET', {}, (data) => {
+        if(data.result === true) {
+          window.location.hash =  '/courses';
+        } else {
+          callAPI(login_origins.backend + '/getUserSession', 'GET', {}, (data) => {
+            if(data['user']) {
+              callAPI(login_origins.backend + '/setUserFromSession', 'GET', {}, (data) => {
+                window.location.hash =  '/courses';
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+
+  getQueries(query_str) {
+		//Map-reduce queries into a single object
+		var queries = query_str.split('&').map((q) => {
+      var parts = q.split('=');
+      var key = parts[0];
+      var val = parts[1];
+      var query = {}; 
+      query[key] = val;
+      return query;
+    }).reduce((acc_obj, q) => {
+      var key = Object.keys(q)[0];
+      acc_obj[key] = q[key];
+      return acc_obj;
+    }, {});
+
+    return queries;
   }
 }
