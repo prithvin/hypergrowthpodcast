032effa06dc7c873e58a22effeeca550f05bcf53
Prithvi Narasimhan <p1narasi@ucsd.edu>
2017-03-06 13:33:46 -0800

    Search page almost done

--- front_end/javascript/search_module/search-videos.js
+++ front_end/javascript/search_module/search-videos.js
@@ -1,4 +1,4 @@
-class SearchVideosClass {
+var SearchVideosClass =  class SearchVideosClass {
     constructor (courseId, mainDiv, searchTerm) {
         this.courseId = courseId;
         this.mainDiv = mainDiv;
@@ -21,9 +21,11 @@ class SearchVideosClass {
 
     loadCard (resultData) {
       loadHTMLComponent("SearchCardModule", function (data) {
-        var mainDiv = $(data);
-        new SearchCardClass(1123123123, $(mainDiv), this.searchTerm, resultData);
-        $(this.mainDiv).find("#search-vid").append(mainDiv);
+        for (var x = 0; x < resultData.length; x++) {
+          var mainDiv = $(data);
+          new SearchCardClass($(mainDiv), this.searchTerm, resultData[x]);
+          $(this.mainDiv).find("#search-vid").append(mainDiv);
+        }
       }.bind(this));
       
     }
--- front_end/javascript/search_module/search_card.js
+++ front_end/javascript/search_module/search_card.js
@@ -1,33 +1,80 @@
-class SearchCardClass {
-    constructor (courseId, mainDiv, searchTerm, data) {
-        this.courseId = courseId;
+var SearchCardClass = class SearchCardClass {
+
+    /*
+        Data: {LectureTime, Matches: {SlideNo, Text, Type, Time (if applicable)}   PodcastId, PodcastImage}
+    */
+    constructor (mainDiv, searchTerm, data) {
         
-        this.card = $(mainDiv);
+        this.mainDiv = $(mainDiv);
+        this.data = data;
+        this.mark = new Mark($(this.mainDiv)[0]);
+        this.searchTerm = searchTerm;
+        
+        // Update DOM stuff
+        $(this.mainDiv).find(".lecture-image-search").attr("src", data['PodcastImage']);
         this.title = $(mainDiv).find(".video-card-title");
-        
-        $(this.title).html(moment(data['Time']).format("dddd, MM/DD"));
 
-        $(this.card).attr("data-podcastid", data['PodcastId']);
-        
-        $(this.card).on("click", function (ev) {
-            window.location.hash = '#/podcast/' + this.getAttribute("data-podcastid");
-        });
+        // Listeners and stuff
+        $(this.title).html(moment(data['LectureTime']).format("dddd, MM/DD"));
+        this.clickedImage();
 
+        
         loadHTMLComponent("SearchCardIndvModule", function (htmlComponent) {
             this.cardModule = htmlComponent;
-            this.appendOCRandAudio(this.card, data);
+            this.appendOCRandAudio(this.mainDiv, data);
         }.bind(this));
+
+        this.mark.mark(searchTerm, { 
+            "caseSensitive" : false, 
+            "separateWordSearch" : false,
+            "exclude": [".pre-slide-data", ".slide-no"]
+        })
         
-        
+    }
+
+    clickedImage () {
+        $(this.mainDiv).find(".lecture-image-search").on("click", function (ev) {
+            window.location.hash = '#/podcast/' + this.data['PodcastId'];
+        }.bind(this));
     }
     
     appendOCRandAudio(cardDiv, data) {    
-        console.log(data);
-        for (var i = 0; i < Math.random() * 8; i ++) {
+        var matches = data.Matches;
+        for (var i = 0; i < matches.length; i ++) {
             var ocrText = $(this.cardModule);
-            ocrText.find(".content-span").html("This is a post " + i);
-            console.log(ocrText);
+            ocrText.find(".content-span").html(this.generateMatch(matches[i].Text));
+            if (matches[i].Type == "AUDIO")
+                ocrText.find(".pre-slide-data").html("Transcription match during");
+            else
+                ocrText.find(".pre-slide-data").html("Slide match on");
+            ocrText.find(".slide-no").attr("data-slide", matches[i].SlideNo).html("Slide " + matches[i].SlideNo);
             $(cardDiv).append(ocrText);
         }
     }   
+
+    generateMatch(text) {
+        var searchTerm = this.searchTerm;
+        var splittedString = text.split(searchTerm);
+        var newString = "";
+        console.log(searchTerm );
+        console.log(text);
+        for (var x = 1; x < splittedString.length; x++) 
+            newString += this.getFiveDiffs(splittedString[x - 1], searchTerm, splittedString[x]);
+        return newString;
+    }
+
+    getFiveDiffs (left, term, right) {
+        var newLeft = left.split(' ').slice(0, 5).join(' ');
+        var term = " " + term + " ";
+
+        var splitRight = right.split(' ');
+        var newRight = splitRight.slice(splitRight.length - 5).join(' ');
+
+        return "..." + newLeft + term  + newRight + "...\n";
+        
+    }
+
+    getFirstFiveString (text) {
+        return ;
+    }
 }
