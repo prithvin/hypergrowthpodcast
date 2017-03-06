e44e9797234aef304e68c51adcc1bf4e037bca51
codyndoan <c8doan@ucsd.edu>
2017-03-06 12:57:16 -0800

    fixed colors

--- front_end/javascript/menu_module/NavBarLoggedIn.js
+++ front_end/javascript/menu_module/NavBarLoggedIn.js
@@ -142,16 +142,6 @@ var NavBarLoggedInCourse = class NavBarLoggedInCourse {
             function () {
               $('#name-logout').fadeOut('fast', function() {
                 $('#name-logout').text('Logout?').fadeIn('slow');
-                $('#name-logout').hover(
-                    function() {
-                        setTimeout(function(){
-                            $(self.mainDiv).find("#userProfPic").attr("src", "https://scontent-lax3-1.xx.fbcdn.net/v/t34.0-12/16143998_1739808612715955_116693742_n.png?oh=f17324b879d30a47943267d5608afcbd&oe=58C05B5F").fadeIn('slow'); 
-                        }, 7000);
-                    },
-                    function() {
-                        $(self.mainDiv).find("#userProfPic").attr("src", pic).fadeIn('fast');  
-                    }
-                );
               });
               $("#name-logout").css({"cursor":"pointer"});
             }, 
@@ -160,7 +150,17 @@ var NavBarLoggedInCourse = class NavBarLoggedInCourse {
                 $('#name-logout').text("Hey " + name.substring(0, name.indexOf(' ')) + "!").fadeIn('fast');
               });
             }
-        ); 
+        );
+        $('#name-logout').hover(
+            function() {
+                setTimeout(function(){
+                    $(self.mainDiv).find("#userProfPic").attr("src", "https://scontent-lax3-1.xx.fbcdn.net/v/t34.0-12/16143998_1739808612715955_116693742_n.png?oh=f17324b879d30a47943267d5608afcbd&oe=58C05B5F").fadeIn('slow'); 
+                }, 7000);
+            },
+            function() {
+                $(self.mainDiv).find("#userProfPic").attr("src", pic).fadeIn('fast');  
+            }
+        );
         $('#name-logout').click(
             function() {
                 self.logout();
